bad24936d17f516b220e46dc26d2d19028427bb3
Echowxsy <echowxsy@gmail.com>
2020-02-16 00:28:33 +0800

    import zh-cn

--- browser/render/lib/languages.js
+++ browser/render/lib/languages.js
@@ -2,7 +2,8 @@ const languages = [
   { name: 'English', code: 'en' },
   { name: 'Tiếng Việt', code: 'vi' },
   // thanks @zbahadir for Turkish translation
-  { name: 'Turkish', code: 'tr' }
+  { name: 'Turkish', code: 'tr' },
+  { name: '简体中文', code: 'zh-cn' }
 ]
 
 module.exports = {
